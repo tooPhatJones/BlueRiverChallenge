9ccc85a72ed85f031247db41043ec21be9aabbf6
david <davidfarley7171@gmail.com>
2020-06-22 18:21:07 -0700

    finished

--- june22test/README.md
+++ june22test/README.md
@@ -1,3 +1,38 @@
+# Technical Exercise - Consume, Filter and Sort raw data for display in HTML.
+## End Goal (highlighted in red): 
+http://drop.ballisty.com/E9IHRGCOubYp 
+
+## API
+API Endpoint: https://applefacilities.review.blueriver.com/index.cfm/_api/json/v1/scv/building/?andOpenGrouping&locationCode%5B0%5D=sqo&or&locationCode%5B2%5D=nwr&or&locationCode%5B4%5D=scv&or&locationCode%5B6%5D=sfo&closeGrouping&fields=buildingname,buildingabbr,lat,lng,black,buildingZone&active=1&cachedwithin=600
+
+## Requirements
+* Buildings should be grouped by Building Zone. This is a property in the "items" building object array.
+* Building Zone should be sorted alphabetically, except for "Other", which should display last.
+* Building Name should be sorted alphabetically.
+* Non Black (black=0) buildings should link to https://applefacilities.review.blueriver.com.
+* Black (black=1) buildings should have no link.
+
+## Notes:
+* CORS has been configured for localhost, localhost:3000, localhost:8080
+* Use any programming language you prefer
+
+## Delivery:
+* You have 1.5 hours to complete this challenge.
+* When complete, please email eddie.ballisty@blueriver.com with either a link to a Git Repo or just zip up your code with a README on how to run.
+
+### Good Luck!
+
+
+
+
+
+
+
+
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -66,3 +101,14 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+
+
+
+
+
+
+
+
+
--- june22test/src/App.js
+++ june22test/src/App.js
@@ -24,23 +24,35 @@ export class App extends React.Component {
         console.log(error);
       });
 
-     let sorted = temp.sort(function(a, b){
-
-      return ('' + a.buildingzone).localeCompare(b.buildingzone);});
-     console.log(sorted)
-
-     let last = sorted[0].buildingzone;
-     let lastindex =0;
-     let Newarray = []
-     for(let i = 0; i < sorted.length; i++){
-      if(sorted[i].buildingzone != last) {
-        Newarray.push(sorted.slice(lastindex, i-1))
+    let sorted = temp.sort(function (a, b) {
+      return ('' + a.buildingzone).localeCompare(b.buildingzone);
+    });
+    
+    //split the array into sub arrays based on the buildingzone
+    let last = sorted[0].buildingzone;
+    let lastindex = 0;
+    let arrayOfArrays = []
+    for (let i = 0; i < sorted.length; i++) {
+      if (sorted[i].buildingzone != last) {
+        arrayOfArrays.push(sorted.slice(lastindex, i))
+        lastindex= i;
         last = sorted[i].buildingzone
       }
     }
-    console.log(Newarray)
-    
-      this.setState({ items: sorted })
+
+    //add other to the end.
+    let firstArrayvalue = arrayOfArrays.shift()
+    console.log(firstArrayvalue)
+    arrayOfArrays.push(firstArrayvalue)
+
+    //sort the sub arrays by buildingname
+    for(let a= 0; a< arrayOfArrays.length; a++ ){
+      arrayOfArrays[a] = arrayOfArrays[a].sort(function (a, b) {
+        return ('' + a.buildingname).localeCompare(b.buildingname);
+      })
+    }
+
+    this.setState({ items: arrayOfArrays })
   }
 
   render() {
@@ -49,11 +61,19 @@ export class App extends React.Component {
         <h1>A list of building stuff from applefacilities</h1>
 
         {this.state.items.map((value, index) => (
-          <div key={index}>
-            <h2>{value.buildingzone}</h2>
-            <h3>{value.buildingname}</h3>
-            {!value.black ?<a href="https://applefacilities.review.blueriver.com">website</a>: null}
-
+          <div key={index+'outer'} className="outer">
+            <h1>{value[0].buildingzone}</h1>
+            <ul>
+              {value.map((sub, index) => (
+                <div key={index+'inner'} className='inner'>
+                  <h3>
+                    {sub.buildingname}
+                    <br/>
+              {!sub.black? <a href ="https://applefacilities.review.blueriver.com">Websight</a>: <i>no websight</i>}
+                  </h3>
+                </div>
+              ))}
+            </ul>
           </div>
         ))}
       </div>
